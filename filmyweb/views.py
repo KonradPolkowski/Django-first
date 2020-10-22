c03f66fded18973fc97c43a603d017cde8fc8434
Konrad Polkowski <konrad.polkovvski@gmail.com>
2020-10-22 14:49:24 +0200

    adding templates and html file (before ORM testing)

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Film
 
-# Create your views here.
+
+def wszystkie_filmy(request):
+    # return HttpResponse("<h1>to jest nasz pierwszy test</h1>")
+    test = "to jest cos we views"
+    filmy = ["Avatar", "Titanic",  "godfaher"]
+    wszystkie = Film.objects.all()
+    return render(request, 'filmy.html', {'filmy': wszystkie, 'text': test})
